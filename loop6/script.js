4494913e151bcccc7c0687607029d7287d1834f0
otabek-oktamov <otabek20040608@gmail.com>
2021-09-03 08:25:45 +0500

    commit

--- loop6/script.js
+++ loop6/script.js
@@ -1,10 +1,10 @@
 const card = document.getElementById("card");
 let y,x;
 document.addEventListener("mousemove", () => {
-    var posX = event.clientX;
-    var posY = event.clientY;
-    x = posX /2
-    y = posY/2
+    var posX = event.clientX /2;
+    var posY = event.clientY /2;
+    x = posX /10
+    y = posY/10
 console.log(x);
     card.style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)";
 });
